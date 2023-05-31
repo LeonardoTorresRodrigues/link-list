1ee98a0d049691fb12b60bd384ebd7c8f53cbcc6
Leonardo Rodrigues <leonardo.t.rodri@gmail.com>
2023-05-31 13:14:46 -0300

    feat: using regex function to get title and link at 'texto.md'

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 import fs from 'fs';
 import chalk from 'chalk';
 
+function extraiLinks(texto) {
+  const regex = /\[([^[\]]*?)\]\((https?:\/\/[^\s?#.].[^\s]*)\)/gm;
+  const capturas = [...texto.matchAll(regex)];
+  const resultados = capturas.map(captura => ({ [captura[1]]: captura[2] }));
+  return resultados;
+}
+
 function trataErro(erro) {
   throw new Error(chalk.red(erro.code, 'não há arquivo no diretório'));
 }
@@ -10,7 +17,7 @@ async function pegaArquivo(caminhoDoArquivo) {
   try {
     const encoding = 'utf-8';
     const texto = await fs.promises.readFile(caminhoDoArquivo, encoding)
-    console.log(chalk.green(texto));
+    console.log(extraiLinks(texto));
   } catch (erro) {
     trataErro(erro);
   } finally {
@@ -19,4 +26,3 @@ async function pegaArquivo(caminhoDoArquivo) {
 }
 
 pegaArquivo('./arquivos/texto.md');
-setInterval(() => pegaArquivo('./arquivos/'), 3000);
