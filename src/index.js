b7e1ce3da2e145f5fa826f5d88de7fa12ec8c678
Leonardo Rodrigues <leonardo.t.rodri@gmail.com>
2023-06-02 14:09:24 -0300

    refactor: exporting 'pegaArquivos' and improving function return

--- src/index.js
+++ src/index.js
@@ -3,9 +3,10 @@ import chalk from 'chalk';
 
 function extraiLinks(texto) {
   const regex = /\[([^[\]]*?)\]\((https?:\/\/[^\s?#.].[^\s]*)\)/gm;
+  // /https?:\/\/[^\s$/.?#].[^\s\)]*/gm [OPÇÃO 2]
   const capturas = [...texto.matchAll(regex)];
   const resultados = capturas.map(captura => ({ [captura[1]]: captura[2] }));
-  return resultados;
+  return resultados.length !== 0 ? resultados : 'não há links no arquivo'
 }
 
 function trataErro(erro) {
@@ -17,7 +18,7 @@ async function pegaArquivo(caminhoDoArquivo) {
   try {
     const encoding = 'utf-8';
     const texto = await fs.promises.readFile(caminhoDoArquivo, encoding)
-    console.log(extraiLinks(texto));
+    return (extraiLinks(texto));
   } catch (erro) {
     trataErro(erro);
   } finally {
@@ -25,4 +26,4 @@ async function pegaArquivo(caminhoDoArquivo) {
   }
 }
 
-pegaArquivo('./arquivos/texto.md');
+export default pegaArquivo;
